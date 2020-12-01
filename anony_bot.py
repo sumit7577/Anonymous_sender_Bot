ffe7a523ef8f5a3ffcd12e284c4ec0b798aa0d41
sumit7577 <asmitkumarr082@gmail.com>
2020-12-01 17:27:32 +0530

    fixed bugs

--- anony_bot.py
+++ anony_bot.py
@@ -1,6 +1,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler,Filters
 import  logging
+import os
 
+PORT = int(os.environ.get("PORT", 5000))
+TOKEN = "1445964032:AAEQjWjtVuRZ94vcTclsBiMgCdAWa7xkp8g"
 updater = Updater(token = "1445964032:AAEQjWjtVuRZ94vcTclsBiMgCdAWa7xkp8g", use_context = True)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -10,7 +13,7 @@ dispatcher = updater.dispatcher
 
 #function
 def start(update, context):
-	context.bot.send_message(chat_id = update.effective_chat.id, text = "hlo i m working")
+	context.bot.send_message(chat_id = update.effective_chat.id, text = "Hey! Master I am anonymou_sender Bot And I m Alive")
 
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
@@ -22,7 +25,9 @@ def echo(update, context):
 message_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(message_handler)
 
-
-
-updater.start_polling()
+if __name__ == "__main__":
+	updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+updater.bot.setWebhook('https://anonymous-boter.herokuapp.com/' + TOKEN)
 
